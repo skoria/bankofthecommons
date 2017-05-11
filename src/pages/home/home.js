89ff23bfc811cfc37d8910631257adf1c817452b
Ale Fernandez <skoria@gmail.com>
2017-05-11 15:21:26 +0200

    Various english language edits
    Edits to the english language where incorrect or hard to understand. "old cooperativist investment problem" is now a bit more explained as per explanation in chat group.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -12,20 +12,20 @@ class Home extends Component {
               <div className='wrapper_istance'>
                 <h2 className='wrapper_title'>The vision</h2>
                 <p>Banking and payment technologies are improving rapidly. <br />
-                Ethical banking cooperatives and democratically run alternative currencies have the challenge of changing ease of use while staying true to their values.<br />
-                Cooperative and solidarity economies needs to spread beyond activist circles by making themselves more attractive and practically usable in everyday life. <br />
-                At same time, decentralized tools that are a part of the blockchain revolution make it possible to decentralize banking and make it more transparent, and these capacities can be used for recovering popular sovereignty in finances, participative budgeting and money creation. <br />
-                In order to make these big improvements happen it is very important to create economies of scale so that we can build shared technologies, integrations and services, that any local financial cooperative or currency project can use, and this is what this European banking cooperative project is about. </p>
+                Ethical banking cooperatives and democratically run alternative currencies have the challenge of improving ease of use for participants whilst staying true to their values.<br />
+                Cooperative and solidarity economies need to spread beyond activist circles by making themselves more attractive and usable in everyday life in practical ways. <br />
+                At same time, the decentralized tools that are a part of the blockchain revolution make it possible to decentralize banking and make it more transparent, and these capacities can be used to recover popular sovereignty in finances, participative budgeting and money creation. <br />
+                In order to make these big improvements happen it is very important to create economies of scale so that we can build shared technologies, integrations and services that any local financial cooperative or currency project can use. This is what this European cooperative banking project is about. </p>
                 <div className='list'>
-                  These are some basic goals that this project should achieve:
+                  These are some basic project goals:
                   <ul>
                     <li>Payment systems without corporate intermediation, between all subjects of the cooperative and solidarity economy.</li>
                     <li>To provide decentralised technologies to the ethical banking and alternative currencies movements.                  </li>
-                    <li>To interconnect the ethical banking and the alternatives currencies movements, with practical applications that simplify their exchanges.</li>
-                    <li>To foster ethical banking and payments possibilities to any part of Europe, and progressively the world.</li>
-                    <li>To reelaborate the meaning of the money creation, solving the old cooperativist investments problem; and creating awareness about how money creation works and can be changed.</li>
+                    <li>To interconnect ethical banking and alternatives currencies movements with practical applications that simplify their exchanges.</li>
+                    <li>To foster ethical banking and payment possibilities to any part of Europe, and progressively the world.</li>
+                    <li>To reelaborate the meaning of money creation, solving the problems that typically affect cooperativist investments(These are characterised by a lack of profit, difficulty with exit strategies and member's rights leading people to prefer private startups); and creating awareness about how money creation works and how it can be changed.</li>
                   </ul>
-                  Some already existing tools and some in development are part of the bank's plan. These include faircoin, fairpay, and the freecoin complementary currencies toolkit amongst others.
+                  The bank's plan involves some tools that already exist and some that are currently under development. These include faircoin, fairpay, and the freecoin complementary currencies toolkit amongst others.
                 </div>
               </div>
             </div>
