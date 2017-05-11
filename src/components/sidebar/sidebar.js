07cb5921cda56a9f0b813c54984d7ca72423096c
Ale Fernandez <skoria@gmail.com>
2017-05-11 14:59:21 +0200

    tiny change
    capital V for vision.

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -7,10 +7,11 @@ class Sidebar extends Component {
       <aside className='sidebar'>
           <span>bank <br />of the <br />commons</span>
           <ul className='sidebar_menu'>
-            <li className='menu_item'><Link to='/' activeClassName='active'> vision</Link></li>
+            <li className='menu_item'><Link to='/' activeClassName='active'>Vision</Link></li>
             <li className='menu_item'><Link to='/roadmap' activeClassName='active'>Roadmap</Link></li>
             <li className='menu_item'><Link to='/membership' activeClassName='active'>Membership</Link></li>
             <li className='menu_item'><Link to='/contact' activeClassName='active'>Contact us</Link></li>
+            <li className='menu_item'><Link to='http://es.bankofthecommons.coop/' activeClassName='active'>Español</Link></li>
           </ul>
         </aside>
     )
